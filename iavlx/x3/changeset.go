e0ebed4dddd506ceac3c145b8468b0bba41f016e
Aaron Craelius <aaronc@users.noreply.github.com>
2025-10-02 21:25:33 -0400

    fix bug

--- iavlx/x3/changeset.go
+++ iavlx/x3/changeset.go
@@ -380,7 +380,7 @@ func (cr *Changeset) TryDispose() bool {
 			cr.files = nil
 			cr.info = nil
 			cr.infoMmap = nil
-			cr.treeStore = nil
+			// DO NOT set treeStore to nil, as deposed changesets should still forward calls to the main tree store
 			return true
 		}
 	}
